731ca86d3271643a91c5c0a2a7aa493717a641d7
tobi <Tobias20.3@hotmail.de>
2025-03-13 15:46:56 +0100

    Improve log parsing

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+use chrono::{DateTime, Duration, NaiveDateTime, TimeZone, Utc};
+
 use clap::Parser;
 use indicatif::{ProgressBar, ProgressStyle};
-use std::collections::HashSet;
 use std::fs::File;
 use std::io::{BufRead, BufReader, Write};
+
+const MAX_PLAYERS: usize = 64;
 
 #[derive(Parser, Debug)]
 #[command(
@@ -24,30 +27,72 @@ struct Args {
     whitelist: Vec<String>,
 }
 
-struct Anonymizer {
-    player_names: HashSet<String>,
-    dropped_lines: usize,
+struct Player {
+    name: Option<String>,
+    client_id: usize,
+    ip: String,
 }
 
-impl Anonymizer {
-    fn new() -> Anonymizer {
-        Anonymizer {
-            player_names: HashSet::new(),
-            dropped_lines: 0,
+struct PlaySession {
+    start: DateTime<Utc>,
+    duration: Option<Duration>,
+    player_name: Option<String>,
+    client_id: usize,
+    client_ip: String,
+}
+
+#[derive(Debug)]
+struct Line {
+    date_time: DateTime<Utc>,
+    group: String,
+    message: String,
+}
+
+struct LogParser {
+    players: [Option<Player>; MAX_PLAYERS],
+}
+
+impl LogParser {
+    fn new() -> LogParser {
+        LogParser {
+            players: [const { None }; MAX_PLAYERS],
         }
     }
 
-    fn process_line(&mut self, group: &str, message: String) -> Option<String> {
-        match group {
-            "chat" => self.process_chat(message),
-            _ => Some(message),
+    pub fn parse_line(line: String) -> Option<Line> {
+        let parts: Vec<&str> = line.split_whitespace().collect();
+
+        // sanity check for syntax
+        if parts.len() < 4 {
+            return None;
         }
+
+        let group = parts[3].strip_suffix(':').unwrap_or(parts[3]); // TODO:
+        let message = parts[4..].join(" ");
+
+        let date_time_string = format!("{} {}", parts[0], parts[1]);
+        let naive_date_time =
+            NaiveDateTime::parse_from_str(&date_time_string, "%Y-%m-%d %H:%M:%S").unwrap();
+        let utc_date_time: DateTime<Utc> = Utc.from_utc_datetime(&naive_date_time);
+
+        Some(Line {
+            date_time: utc_date_time,
+            group: group.to_string(),
+            message,
+        })
     }
 
-    fn process_chat(&mut self, message: String) -> Option<String> {
+    fn process_line(&mut self, line: &Line) {
+        match line.group.as_str() {
+            "chat" => self.process_chat(&line.message),
+            _ => {}
+        };
+    }
+
+    fn process_chat(&mut self, message: &str) {
         // chat messages dont start with ***, we drop those
         if !message.starts_with("***") {
-            return None;
+            return;
         }
 
         // extract player names from join messages
@@ -55,18 +100,16 @@ impl Anonymizer {
             if let Some(start) = message.find('\'') {
                 if let Some(end) = message[start + 1..].find('\'') {
                     let player_name = &message[start + 1..start + 1 + end];
-                    self.player_names.insert(player_name.to_string());
+                    // self.player_names.insert(player_name.to_string());
                 }
             }
         }
-
-        Some(message)
     }
 }
 
 fn main() -> std::io::Result<()> {
     let args = Args::parse();
-    let mut anon = Anonymizer::new();
+    let mut anon = LogParser::new();
 
     let count_file = File::open(&args.input)?;
     let count_reader = BufReader::new(count_file);
@@ -87,34 +130,10 @@ fn main() -> std::io::Result<()> {
         let line = line?;
         progress.inc(1);
 
-        // sanity check for syntax
-        let parts: Vec<&str> = line.split_whitespace().collect();
-        if parts.len() < 4 {
-            progress.println(format!(
-                "Warning: Line {} does not have expected format: {}",
-                line_number + 1,
-                line
-            ));
-            continue;
-        }
-
-        let date = parts[0];
-        let time = parts[1];
-        let group = parts[3].strip_suffix(':').unwrap_or(parts[3]); // TODO:
-        let message = parts[4..].join(" ");
-
-        // check group whitelist
-        if !args.whitelist.contains(&group.to_string()) {
-            continue;
-        }
-
-        let processed_message = anon.process_line(group, message);
-
-        if let Some(message) = processed_message {
-            let output_line = format!("{} {} I {}: {}", date, time, group, message);
-            writeln!(output_file, "{}", output_line)?;
-        }
+        let processed_message = LogParser::parse_line(line);
+        dbg!(processed_message);
     }
+
     progress.finish();
     Ok(())
 }
